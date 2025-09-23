087aa79935b1fb897f078036bed01ff883822dcd
jonsoe1 <jon@allye.com>
2025-09-23 22:02:17 +0100

    initial commit (v2)

---
+++ Bearing_Calculations.m
@@ -0,0 +1,32 @@
+%OUR 5 Motor Bearing Calculations
+
+ChainForce = 16000; %Max force on chain as outlined by chain size excel (N)
+%dimensions 'a' and 'b' set via inspection of the solidworks assembly - rough%
+a = 90e-3; %distance from sprocket to the near side bearing - see dimension 'a' in drawing (m)
+b = 210e-3; %distance from sprocket to the far side bearing - see dimension 'b' in drawing (m)
+
+RadialForce_A = ((b)/(b-a))*ChainForce; %The radial force experienced by Bearing A - the near side bearing (N)
+RadialForce_B = ((a)/(b-a))*ChainForce; %The radial force experienced by Bearing B - the far side bearing (N)
+
+disp(['Radial force experienced by bearing A = ', num2str(RadialForce_A), 'N'])
+disp(['Radial force experienced by bearing B = ', num2str(RadialForce_B), 'N'])
+
+%Basic Dynamic Loading Rating
+
+P = max(RadialForce_A,RadialForce_B) * 0.5; %the equivalent dynamic bearing load - approx 50% of max (N)
+n = 4000; % rotational speed the bearing will most commonly be ran at (rpm)
+L = 100; %Life rating of bearing (hours) - where 90% of bearings fail
+p = 3; %Life exponent (3 for ball bearings)
+C = P * ((60*n*L)/10^6)^(1/p); %Basic Dynamic Load Rating (N)
+disp(['Required basic dynamic load rating of bearing = ', num2str(C), 'N'])
+
+%Chosen Bearing
+%Rated_RadialForce = 20000; %What is the bearing radial force rated at (N)
+Rated_ID = 35e-3;
+
+%RadialForce_SF = Rated_RadialForce/RadialForce_A; %The safety factor between what radial force the bearing is rated for and how much it is experiencing
+%disp(['Bearing radial force factor of safety = ', num2str(RadialForce_SF)])
+
+[Max_ShaftVMStress, Shaft_SF] = SplinedShaft_Function(Rated_ID);
+%disp(['Max Shaft Shear Stress = ', num2str(Max_ShaftShearStress/1e6), 'MPa'])
+%disp(['Shaft Safety Factor = ', num2str(Shaft_SF)])
---
+++ SplinedShaft_Function.m
@@ -0,0 +1,56 @@
+function [VM_stress, safety_factor] = SplinedShaft_Function(Shaft_OD)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+% OUR5 motor shaft sizing
+
+%% Constants
+
+% MOTOR DATA
+% YASA P400RS 12T
+Peak_Torque = 250;  % (Nm)
+Peak_BendingMoment = 1440; % (Nm)
+
+% SHAFT DIMENSIONS
+% shaft is a solid cylinder
+% Spline dimensions for reference:
+% d_major = 35.59;  % (mm)
+% d_minor = 33.38;  % (mm)
+%Shaft_OD = 34e-3;  % shaft diameter (m)
+L = 0.2;  % shaft length (m)
+
+R = Shaft_OD/2;  % shaft radius (m)
+
+% MATERIAL PROPERTIES 
+% (mild steel from HLT)
+G = 80e9;  % shear modulus of material (Pa)
+Sy = 240e6;  % yield strength of material (Pa)
+
+%% Calculations
+
+%yield_shear_stress = Sy/sqrt(3);  % von Mises max shear stress in pure shear yielding
+%disp(['Yield Shear Stress of shaft material = ', num2str(yield_shear_stress/1e6), ' MPa'])
+
+%Torsion
+J = pi/32 * (Shaft_OD^4);  % polar moment of inertia (m^4) for solid circular shaft
+I = pi/64 * (Shaft_OD^4); %second moment for bending (m^4) for solid circular shaft
+max_shear_stress = ((Peak_Torque * R)/J);  % shear stress at outer surface (max) in Pa
+disp(['Max Shear Stress in shaft = ',num2str(max_shear_stress/1e6), ' MPa'])
+%Bending
+max_bending_stress = ((Peak_BendingMoment*R)/I);
+disp(['Max Bending Stress in shaft = ',num2str(max_bending_stress/1e6), ' MPa'])
+%Von misses
+VM_stress = sqrt((max_bending_stress^2) + (3*(max_shear_stress)^2));
+disp(['Von Misses Stress = ',num2str(VM_stress/1e6), ' MPa'])
+%Safety Factor
+safety_factor = Sy/VM_stress;
+disp(['Factor of Safety combined Bending and Torsion = ',num2str(safety_factor)])
+
+twist_angle_rad = ((Peak_Torque*L)/(J*G));
+twist_angle_deg = twist_angle_rad * 180/pi;
+disp(['Twist angle under peak torque = ', num2str(twist_angle_deg), ' deg'])
+
+
+% Safety factor calculation (based on max shear stress)
+%safety_factor = yield_shear_stress / max_shear_stress;
+%disp(['Safety factor (shear) = ', num2str(safety_factor)])
+end

07b0ed50483003c4ede750d080f618119ae06e7e
jsoep <jsoepadmo@hotmail.sg>
2025-11-11 15:29:41 +0000

    update with latest dimensions from CAD

--- Bearing_Calculations.m
+++ Bearing_Calculations.m
@@ -9,15 +9,15 @@ driven_teeth = 47;
 dist = 240.25;  % mm - center to center distance between sprockets
 % Shaft
 shaft_OD = 35;  % mm
-shaft_length = 143.704;  % mm - between sprocket and motor
+shaft_length = 121;  % mm - between sprocket and motor
 shaft_material = "4340";  % material of the shaft
 
 %% Bearing radial force calculations
 chain_force = get_chain_force(chain, driving_teeth, driven_teeth, dist, motor_torque);
 
 %dimensions 'a' and 'b' set via inspection of the onshape assembly - rough
-dist_a = 56.704;  % distance from sprocket to the near side bearing - see dimension 'a' in drawing (mm)
-dist_b = 189.404;  % distance from sprocket to the far side bearing - see dimension 'b' in drawing (mm)
+dist_a = 41.25;  % distance from sprocket to the near side bearing - see dimension 'a' in drawing (mm)
+dist_b = 177.8;  % distance from sprocket to the far side bearing - see dimension 'b' in drawing (mm)
 
 radial_force_a = ((dist_b)/(dist_b-dist_a))*chain_force;  % radial force experienced by Bearing A - the near side bearing (kN)
 radial_force_b = ((dist_a)/(dist_b-dist_a))*chain_force;  % radial force experienced by Bearing B - the far side bearing (kN)
